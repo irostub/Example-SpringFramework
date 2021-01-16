16be85d9a8be2fa222ae7179ad736bcc8dc90ed5
irostub <irostub@gmail.com>
2021-01-16 22:59:49 +0900

    Add api for object pass to json
    delete HelloMVC.java
    use HelloController instead of HelloMVC.java

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String HelloMvc(@RequestParam("text") String text, Model model) {
+        model.addAttribute("data", text);
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String HelloString(@RequestParam("name") String name) {
+        return name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello HelloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
