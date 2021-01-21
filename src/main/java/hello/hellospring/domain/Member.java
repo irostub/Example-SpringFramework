0034d0f4192fbb9231ed0815e00ba02db03df955
irostub <irostub@gmail.com>
2021-01-21 12:04:22 +0900

    Use jpa anotation

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,19 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//Jpa와 연동
+@Entity
 public class Member {
+
+    //Id 어노테이션 : PK 세팅, GeneratedValue 어노테이션 : IDENTITY&AutoIncrement와 같음
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //DB에 name이란 이름으로 컬럼명을 설정하지 않았다면 @Coloum(name="") 어노테이션으로 매핑가능
     private String name;
 
     public String getName() {
