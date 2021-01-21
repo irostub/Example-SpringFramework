e0a8a2ea056b7a0975692ffd0b560c5f4c7ef0ba
irostub <irostub@gmail.com>
2021-01-21 12:19:04 +0900

    Use spring data jpa instead of jpa

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,27 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
     //private final DataSource dataSource;
-    private EntityManager em;
+    //private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*@Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
-    }
+    }*/
 
     /*public SpringConfig(DataSource d) {
         this.dataSource = d;
@@ -27,15 +29,16 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         //MemoryMemberRepository 메모리 데이터레포 대신 이제 실제 DB Jdbc을 사용하고자 할 때, 다형성 실현
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        //return new JpaMemberRepository(em);
     }
+    */
 }
