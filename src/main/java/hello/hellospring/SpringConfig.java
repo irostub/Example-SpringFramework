10ef7c3febc83279bc36401246ca3e732a7ea18d
irostub <irostub@gmail.com>
2021-01-21 12:05:40 +0900

    Use entity manager instead of data source

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,29 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    //private final DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource d) {
-        this.dataSource = d;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
+    /*public SpringConfig(DataSource d) {
+        this.dataSource = d;
+    }*/
 
     @Bean
     public MemberService memberService(){
@@ -31,6 +35,7 @@ public class SpringConfig {
         //MemoryMemberRepository 메모리 데이터레포 대신 이제 실제 DB Jdbc을 사용하고자 할 때, 다형성 실현
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
